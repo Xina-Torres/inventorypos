4a2a1cc01e23dd0e197b425669c628d406bd5930
Maria Xina Rae Torres <xina.torres@gmail.com>
2020-01-31 17:48:12 +0800

    Login Part 1, connect code to DB

--- connectdb.php
+++ connectdb.php
@@ -1,7 +1,6 @@
 <?php
     try{
         $pdo = new PDO('mysql:host=localhost;dbname=pos_db','root','');
-    echo 'Connection Successful';
     }catch(PDOException $f){
            echo $f->getmessage();
     }
