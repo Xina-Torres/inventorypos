4ee361760ffcb2463bda41d65e93b8b046d8d08f
Maria Xina Rae Torres <xina.torres@gmail.com>
2020-02-13 18:35:59 +0800

    Reformatted category and header

--- category.php
+++ category.php
@@ -1,140 +1,264 @@
 <?php
-    include_once 'connectdb.php';
-    session_start();
- if($_SESSION['useremail']=="" || $_SESSION['role']=="User"){
-        header('location:index.php');
-    }
-    include_once 'header.php';
+
+
+include_once'connectdb.php';
+
+session_start();
+
+if($_SESSION['useremail']=="" OR $_SESSION['role']=="User"){
+    
+    
+    header('location:index.php');
+}
+
+
+include_once'header.php';
+
+
+
+
 
 if(isset($_POST['btnsave'])){
-    $category=$_POST['txtcategory'];
-    if(empty($category)){
-        $error='<script type="text/javascript">
-                jQuery(function validation(){
-                    swal({
-                        title: "Field is Empty",
-                        text: "Please fill field.",
-                        icon: "error",
-                        button: "Ok",
-});
-        });
-            </script>';
-        echo $error;
-    }
     
-    if(!isset($error)){
-        $insert=$pdo->prepare("insert into tbl_category(category) values(:category)");
-        $insert->bindParam(':category',$category);
-        if($insert->execute()){
-            echo '<script type="text/javascript">
-                jQuery(function validation(){
-                    swal({
-                        title: "Added!",
-                        text: "Your Category is Added.",
-                        icon: "success",
-                        button: "Ok",
+$category = $_POST['txtcategory'];
+    
+    
+if(empty($category)){
+    
+ $error='<script type="text/javascript">
+jQuery(function validation(){
+
+
+swal({
+  title: "Feild is Empty!",
+  text: "Please Fill Feild!!",
+  icon: "error",
+  button: "Ok",
 });
-        });
-            </script>';
-        }else{
-             echo '<script type="text/javascript">
-                jQuery(function validation(){
-                    swal({
-                        title: "Error",
-                        text: "Query Fail.",
-                        icon: "error",
-                        button: "Ok",
+
+
 });
-        });
-            </script>';
-        }
-    }
-}//btnadd ends here
+
+</script>';   
+    
+  echo $error;  
+    
+    
+    
+}
+    
+    
+if(!isset($error)){
+    
+$insert=$pdo->prepare("insert into tbl_category(category) values(:category)");
+    
+$insert->bindParam(':category',$category); 
+    
+if($insert->execute()){
+   
+    echo '<script type="text/javascript">
+jQuery(function validation(){
+
+
+swal({
+  title: "Added!",
+  text: "Your Category is Added!",
+  icon: "success",
+  button: "Ok",
+});
+
+
+});
+
+</script>';
+    
+    
+    
+    
+}else{
+ echo '<script type="text/javascript">
+jQuery(function validation(){
+
+
+swal({
+  title: "Error",
+  text: "Query Fail!",
+  icon: "error",
+  button: "Ok",
+});
+
+
+});
+
+</script>';
+    
+}    
+}        
+}// btnadd end here
+
 if(isset($_POST['btnupdate'])){
-    $category=$_POST['txtcategory'];
-    $id=$_POST['txtid'];
-    if(empty($category)){
-        $errorupdate='<script type="text/javascript">
-                jQuery(function validation(){
-                    swal({
-                        title: "Error",
-                        text: "Field is Empty: Please Enter category.",
-                        icon: "error",
-                        button: "Ok",
-});
-        });
-            </script>';
-        echo $errorupdate;
-    }//btn update
-    if(isset($errorupdate)){
-        $update=$pdo->prepare("update tbl_category set category=:category where catid=".$id);
-        $update->bindParam(':category',$category);
-        if($update->execute()){
-            echo 
-            '<script type="text/javascript">
-                jQuery(function validation(){
-                    swal({
-                        title: "Updated!",
-                        text: "Your Category is Updated",
-                        icon: "success",
-                        button: "Ok",
-});
-        });
-            </script>';
-        }else{
-            echo
-            '<script type="text/javascript">
-                jQuery(function validation(){
-                    swal({
-                        title: "Error",
-                        text: "Your Category in not Updated",
-                        icon: "error",
-                        button: "Ok",
-});
-        });
-            </script>';
-        }
-    }
     
-}//btnupdate end
+   $category = $_POST['txtcategory'];
+    $id = $_POST['txtid'];
+    
+if(empty($category)){
+   
+$errorupdate='<script type="text/javascript">
+jQuery(function validation(){
+
+
+swal({
+  title: "Error",
+  text: "Feild is empty : please enter category!",
+  icon: "error",
+  button: "Ok",
+});
+
+
+});
+
+</script>';    
+    
+    
+  echo $errorupdate; 
+    
+} 
+    
+if(!isset($errorupdate)){ 
+
+$update=$pdo->prepare("update tbl_category set category=:category where catid=".$id);
+    
+$update->bindParam(':category',$category); 
+    
+if($update->execute()){
+   echo '<script type="text/javascript">
+jQuery(function validation(){
+
+
+swal({
+  title: "Updated!",
+  text: "Your Category is Updated!",
+  icon: "success",
+  button: "Ok",
+});
+
+
+});
+
+</script>';
+       
+    
+    
+}else{
+    
+      echo '<script type="text/javascript">
+jQuery(function validation(){
+
+
+swal({
+  title: "Error!",
+  text: "Your Category is  Not Updated!",
+  icon: "error",
+  button: "Ok",
+});
+
+
+});
+
+</script>';
+    
+    
+    
+    
+}    
+
+
+
+
+}  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+} // btn update code end here
+
+
 
 if(isset($_POST['btndelete'])){
-    $delete=$pdo->prepare("delete from tbl_category where catid=".$_POST['btndelete']);
-    if($delete->execute()){
-        echo  '<script type="text/javascript">
-                jQuery(function validation(){
-                    swal({
-                        title: "Updated!",
-                        text: "Your Category is Deleted",
-                        icon: "success",
-                        button: "Ok",
+    
+ $delete=$pdo->prepare("delete from tbl_category where catid=".$_POST['btndelete']); 
+    
+  
+    
+    
+   if($delete->execute()){
+       
+       echo '<script type="text/javascript">
+jQuery(function validation(){
+
+
+swal({
+  title: "Deleted!",
+  text: "Your Category is Deleted!",
+  icon: "success",
+  button: "Ok",
 });
-        });
-            </script>';
-    }else{
-        echo  '<script type="text/javascript">
-                jQuery(function validation(){
-                    swal({
-                        title: "Erro!",
-                        text: "Your Category is Not Updated",
-                        icon: "error",
-                        button: "Ok",
+
+
 });
-        });
-            </script>';
-    }
+
+</script>'; 
+       
+   }else{
+       echo '<script type="text/javascript">
+jQuery(function validation(){
+
+
+swal({
+  title: "Error!",
+  text: "Your Category is Not Deleted!",
+  icon: "success",
+  button: "Ok",
+});
+
+
+});
+
+</script>';
+       
+       
+       
+       
+       
+   } 
+    
     
 }
 
 
+
+
+
+
+
+
+
+
 ?>
+
+
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>
             Category
-            <!--   <small>Optional description</small>-->
+            <small></small>
         </h1>
         <ol class="breadcrumb">
             <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
@@ -148,6 +272,7 @@ if(isset($_POST['btndelete'])){
         <!--------------------------
         | Your Page Content Here |
         -------------------------->
+
         <div class="box box-warning">
             <div class="box-header with-border">
                 <h3 class="box-title">Category Form</h3>
@@ -155,41 +280,70 @@ if(isset($_POST['btndelete'])){
             <!-- /.box-header -->
             <!-- form start -->
 
+
+
+
             <div class="box-body">
                 <form role="form" action="" method="post">
                     <?php
-                    if(isset($_POST['btnedit'])){
-                        $select=$pdo->prepare("select * from tbl_category where catid=".$_POST['btnedit']);
-                        $select->execute();
-                        
-                        if($select){
-                            $row=$select->fetch(PDO::FETCH_OBJ);
-                           echo '   <div class="col-md-4">
+        if(isset($_POST['btnedit'])){
+            
+    $select=$pdo->prepare("select * from tbl_category where catid=".$_POST['btnedit']); 
+    $select->execute();
+    if($select){
+    $row =$select->fetch(PDO::FETCH_OBJ);    
+         echo' <div class="col-md-4">
+                                 
+                   <div class="form-group">
+                  <label >Category</label>
+<input type="hidden" class="form-control" value="'.$row->catid.'" name="txtid"  placeholder="Enter Category" >
+                  
+                  
+<input type="text" class="form-control" value="'.$row->category.'" name="txtcategory"  placeholder="Enter Category" >
+                </div>
+                
+    <button type="submit" class="btn btn-info" name="btnupdate">Update</button>
+                   
+              </div>'; 
+        
+        
+        
+        
+        
+    }        
+            
+          
+            
+            
+            
+            
+            
+        }else{
+            
+        echo' <div class="col-md-4">
+                                 
+                   <div class="form-group">
+                  <label >Category</label>
+                  <input type="text" class="form-control" name="txtcategory" placeholder="Enter Category" >
+                </div>
+                
+                 <button type="submit" class="btn btn-warning" name="btnsave">Save</button>
+                   
+              </div>';    
+            
+            
+        }          
+                  
+                  
+         ?>
 
-                        <div class="form-group">
-                            <label>Category</label>
-                            
-                            <input type="hidden" class="form-control" name="txtid" value="'.$row->catid.'"  placeholder="Enter Category">
-                            <input type="text" class="form-control" name="txtcategory" value="'.$row->category.'"  placeholder="Enter Category">
-                        </div>
-                        <button type="submit" name="btnsave" class="btn btn-info" name="btnupdate">Update</button>
 
-                    </div>';  
-                        }
-                    }else{
-                        echo '   <div class="col-md-4">
 
-                        <div class="form-group">
-                            <label>Category</label>
-                            <input type="text" class="form-control" name="txtcategory" placeholder="Enter Category">
-                        </div>
-                        <button type="submit" name="btnsave" class="btn btn-warning">Save</button>
 
-                    </div>';
-                    }
-                    ?>
+
 
                     <div class="col-md-8">
+
                         <table id="tablecategory" class="table table-striped">
                             <thead>
                                 <tr>
@@ -198,28 +352,31 @@ if(isset($_POST['btndelete'])){
                                     <th>EDIT</th>
                                     <th>DELETE</th>
                                 </tr>
+
                             </thead>
                             <tbody>
-                                <?php 
-                                
-                                $select=$pdo->prepare("select * from tbl_category order by catid desc");
-                                $select->execute();
-                                while($row=$select->fetch(PDO::FETCH_OBJ)){
-                                echo '<tr>
-                                <td>'.$row->catid.'</td>
-                                <td>'.$row->category.'</td>
-                                <td>
-                                <button type="submit" value="'.$row->catid.'" class="btn btn-success" name="btnedit">Edit</button>
-                                </td>
-                                <td>
-                                <button type="submit" value="'.$row->catid.'" class="btn btn-danger" name="btndelete">Delete</button>
-                                </td>
-                               </tr>';
-                                }
-
-                                ?>
-
-
+                                <?php
+    $select=$pdo->prepare("select * from tbl_category order by catid desc");
+    $select->execute();
+  while($row=$select->fetch(PDO::FETCH_OBJ)){
+    
+echo' <tr>
+    <td>'.$row->catid.'</td>
+    <td>'.$row->category.'</td>
+    
+    <td>
+      <button type="submit" value='.$row->catid.' class="btn btn-success" name="btnedit">Edit</button>
+    </td>
+    
+    <td>
+        <button type="submit" value="'.$row->catid.'" class="btn btn-danger" name="btndelete">Delete</button>
+    </td>
+   
+     </tr>';    
+    
+}            
+    
+   ?>
 
                             </tbody>
                         </table>
@@ -233,17 +390,28 @@ if(isset($_POST['btndelete'])){
             </div>
 
         </div>
+
+
+
+
+
     </section>
     <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
-<!--Call this single function -->
+
+
 <script>
     $(document).ready(function() {
         $('#tablecategory').DataTable();
     });
 
 </script>
+
+
+
 <?php
-   include_once 'footer.php';
+
+include_once'footer.php';
+
 ?>
--- header.php
+++ header.php
@@ -8,28 +8,47 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Admin | Dashboard</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
     <!-- jQuery 3 -->
     <script src="bower_components/jquery/dist/jquery.min.js"></script>
     <!-- Bootstrap 3.3.7 -->
     <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-    <!-- iCheck -->
-    <script src="plugins/iCheck/icheck.min.js"></script>
+
+
+    <!-- AdminLTE App -->
+    <script src="dist/js/adminlte.min.js"></script>
+
     <script src="bower_components/sweetalert/sweetalert.js"></script>
+
+
+
+
     <!-- DataTables -->
     <script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
     <script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-    <script scr="dis/js/adminlte.min.js"></script>
-    <title>Admin | Dashboard</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+    <script src="Chart.js-2.8.0/dist/Chart.min.js"></script>
+
+
     <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+
+    <!-- Select2 -->
+    <link rel="stylesheet" href="bower_components/select2/dist/css/select2.min.css">
+
     <!-- Font Awesome -->
     <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
     <!-- Ionicons -->
     <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
     <!-- Theme style -->
     <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
+        page. However, you can choose any other skin. Make sure you
+        apply the skin class to the body tag so the changes take effect. -->
     <link rel="stylesheet" href="dist/css/skins/skin-blue.min.css">
+
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -42,7 +61,28 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
     <!-- DataTables -->
     <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+
+    <!-- daterange picker -->
+    <link rel="stylesheet" href="bower_components/bootstrap-daterangepicker/daterangepicker.css">
+    <!-- bootstrap datepicker -->
+    <link rel="stylesheet" href="bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+
+
+    <script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+    <!-- bootstrap datepicker -->
+    <script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+    <!-- bootstrap color picker -->
+
+    <!-- iCheck for checkboxes and radio inputs -->
+    <link rel="stylesheet" href="plugins/iCheck/all.css">
+
+    <!-- iCheck 1.0.1 -->
+    <script src="plugins/iCheck/icheck.min.js"></script>
+
+    <!-- Select2 -->
+    <script src="bower_components/select2/dist/js/select2.full.min.js"></script>
 </head>
+
 <!--
 BODY TAG OPTIONS:
 =================
@@ -75,7 +115,7 @@ desired effect
                 <!-- mini logo for sidebar mini 50x50 pixels -->
                 <span class="logo-mini"><b>P</b>OS</span>
                 <!-- logo for regular state and mobile devices -->
-                <span class="logo-lg"><b>Inventory</b>POS</span>
+                <span class="logo-lg"><b>INVENTORY</b>-POS</span>
             </a>
 
             <!-- Header Navbar -->
@@ -87,6 +127,7 @@ desired effect
                 <!-- Navbar Right Menu -->
                 <div class="navbar-custom-menu">
                     <ul class="nav navbar-nav">
+                        <!-- Messages: style can be found in dropdown.less-->
                         <!-- User Account Menu -->
                         <li class="dropdown user user-menu">
                             <!-- Menu Toggle Button -->
@@ -94,7 +135,7 @@ desired effect
                                 <!-- The user image in the navbar-->
                                 <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
                                 <!-- hidden-xs hides the username on small devices so only the image appears. -->
-                                <span class="hidden-xs"><?php echo $_SESSION['username']?></span>
+                                <span class="hidden-xs"><?php echo $_SESSION['username'];?></span>
                             </a>
                             <ul class="dropdown-menu">
                                 <!-- The user image in the menu -->
@@ -102,19 +143,22 @@ desired effect
                                     <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
                                     <p>
-                                        <?php echo $_SESSION['useremail']?>
+                                        <?php echo $_SESSION['useremail'];?>
+                                        <small>Member since Nov. 2012</small>
                                     </p>
                                 </li>
                                 <!-- Menu Body -->
                                 <li class="user-body">
                                     <div class="row">
+
+
                                     </div>
                                     <!-- /.row -->
                                 </li>
                                 <!-- Menu Footer-->
                                 <li class="user-footer">
                                     <div class="pull-left">
-                                        <a href="changepassword.php" class="btn btn-default btn-flat">Change Password</a>
+                                        <a href="changepassword.php" class="btn btn-default btn-flat">Change password</a>
                                     </div>
                                     <div class="pull-right">
                                         <a href="logout.php" class="btn btn-default btn-flat">Sign out</a>
@@ -123,6 +167,7 @@ desired effect
                             </ul>
                         </li>
                         <!-- Control Sidebar Toggle Button -->
+
                     </ul>
                 </div>
             </nav>
@@ -139,7 +184,7 @@ desired effect
                         <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
                     </div>
                     <div class="pull-left info">
-                        <p>WELCOME-<?php echo $_SESSION['username']?></p>
+                        <p>WELCOME-<?php echo $_SESSION['username'];?></p>
                         <!-- Status -->
                         <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
                     </div>
@@ -159,15 +204,58 @@ desired effect
 
                 <!-- Sidebar Menu -->
                 <ul class="sidebar-menu" data-widget="tree">
+
+
                     <li><a href="dashboard.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+
                     <li><a href="category.php"><i class="fa fa-list-alt"></i> <span>Category</span></a></li>
+
+                    <li><a href="addproduct.php"><i class="fa fa-product-hunt"></i> <span>Add Product</span></a></li>
+
+                    <li><a href="productlist.php"><i class="fa fa-th-list"></i> <span>Productlist</span></a></li>
+
+                    <li><a href="createorder.php"><i class="fa fa-first-order"></i> <span>Createorder</span></a></li>
+
+                    <li><a href="orderlist.php"><i class="fa fa-list-ul"></i> <span>Orderlist</span></a></li>
+
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-table"></i> <span>Sales Report</span>
+                            <span class="pull-right-container">
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </span>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li><a href="tablereport.php"><i class="fa fa-circle-o"></i>Table Report</a></li>
+                            <li><a href="graphreport.php"><i class="fa fa-circle-o"></i>Grpah Report</a></li>
+                        </ul>
+                    </li>
+
+
+
+
+
+
                     <li><a href="registration.php"><i class="fa fa-registered"></i> <span>Registration</span></a></li>
 
-                    <ul class="treeview-menu">
-                        <li><a href="#">Link in level 2</a></li>
-                        <li><a href="#">Link in level 2</a></li>
-                    </ul>
-                    </li>
+
+
+
+
+
+                    <!--
+        <li class="treeview">
+          <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+              </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="#">Link in level 2</a></li>
+            <li><a href="#">Link in level 2</a></li>
+          </ul>
+        </li>
+-->
                 </ul>
                 <!-- /.sidebar-menu -->
             </section>
